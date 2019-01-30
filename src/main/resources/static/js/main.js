fdb5535c517f2e46cc6432c99cb3386e31ddd434
unknown <ffcojaviercarrillo@gmail.com>
2019-01-29 21:14:35 -0600

    Paginas a las paginas de inicio de cada rol

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -10,40 +10,20 @@
 	
 	
 	//Pagination
-		// -----------------------------------------------------------------
-		$('#demo-foo-pagination').footable();
-		$('#demo-show-entries').change(function (e) {
-			e.preventDefault();
-			var pageSize = $(this).val();
-			$('#demo-foo-pagination').data('page-size', pageSize);
-			$('#demo-foo-pagination').trigger('footable_initialized');
-		});
+	$('#demo-foo-pagination').footable();
+	$('#demo-show-entries').change(function (e) {
+		e.preventDefault();
+		var pageSize = $(this).val();
+		$('#demo-foo-pagination').data('page-size', pageSize);
+		$('#demo-foo-pagination').trigger('footable_initialized');
+	});
 
-		
-		// Add & Remove Row
-		var addrow = $('#tableId');
-		addrow.footable().on('click', '.delete-row-btn', function() {
-
-			//get the footable object
-			var footable = addrow.data('footable');
-
-			//get the row we are wanting to delete
-			var row = $(this).parents('tr:first');
-
-			//delete the row
-			footable.removeRow(row);
-		});
-	    var addrow = $('#demo-foo-addrow2');
-		addrow.footable().on('click', '.delete-row-btn', function() {
-
-			//get the footable object
-			var footable = addrow.data('footable');
-
-			//get the row we are wanting to delete
-			var row = $(this).parents('tr:first');
-
-			//delete the row
-			footable.removeRow(row);
-		});
+	
+	var addrow = $('#tableId');
+	addrow.footable().on('click', '.delete-row-btn', function() {
+		var footable = addrow.data('footable');
+		var row = $(this).parents('tr:first');
+		footable.removeRow(row);
+	});
     
   });
