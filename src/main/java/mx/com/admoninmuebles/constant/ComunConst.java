c95005bf14823509379b027d120d9cba98697f7f
unknown <ffcojaviercarrillo@gmail.com>
2019-02-27 18:21:52 -0600

    Numeracion de id en tablas autogenerado, paginacion en tablas a 15

--- src/main/java/mx/com/admoninmuebles/constant/ComunConst.java
+++ src/main/java/mx/com/admoninmuebles/constant/ComunConst.java
@@ -7,6 +7,7 @@ public final class ComunConst {
 	}
 	
 	public static final Integer TAMANIO_5_MB = 5000000;
+	public static final Integer TAMANIO_PAGINACION = 15;
 	
 	public static final String USUARIO_SISTEMA = "Sistema";
 	public static final String CADENA_VACIA = "";
